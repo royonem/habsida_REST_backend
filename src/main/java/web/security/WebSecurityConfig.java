4c4250bb1ac557dbfd47ccf4f00f890e68c0ea7f
royonem <roywmyung@gmail.com>
2026-03-10 12:04:36 +0900

    feat(config): add AuthManager bean

--- src/main/java/web/security/WebSecurityConfig.java
+++ src/main/java/web/security/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package web.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,5 +26,11 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // stateless for REST
                 );        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
