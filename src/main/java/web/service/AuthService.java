0eb1fea8eeef63c7bc04a0b5938090d75602a862
royonem <roywmyung@gmail.com>
2026-03-15 16:23:50 +0900

    feat(service): added try catch for login and get roleNames logic

--- src/main/java/web/service/AuthService.java
+++ src/main/java/web/service/AuthService.java
@@ -3,11 +3,15 @@ package web.service;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import web.dto.CreateUserDTO;
 import web.dto.LoginRequestDTO;
 import web.dto.LoginResponseDTO;
 import web.security.UserPrincipal;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthService {
@@ -21,18 +25,26 @@ public class AuthService {
     }
 
     public LoginResponseDTO login(LoginRequestDTO request) {
-        Authentication authentication =
-                authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUsername(),
-                        request.getPassword()
-                )
-        );
-        UserPrincipal principal =
-                (UserPrincipal) authentication.getPrincipal();
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
 
-        String token = "temp-token";
-        return new LoginResponseDTO(principal.getUsername(), token);
+            UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+
+            Set<String> roleNames = principal.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toSet());
+
+            String token = "temp-token";
+            return new LoginResponseDTO(principal.getUsername(), token, roleNames);
+
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Invalid username or password");
+        }
     }
 
     public void register(CreateUserDTO dto) {
