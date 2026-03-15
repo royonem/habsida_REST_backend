7a2a64cef0e72876bc869255cf3224fd39e7630a
royonem <roywmyung@gmail.com>
2026-03-15 14:58:41 +0900

    feat(config): added cors config class. also minor import changes to controller classes

--- src/main/java/web/controller/AuthController.java
+++ src/main/java/web/controller/AuthController.java
@@ -1,10 +1,7 @@
 package web.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web.dto.CreateUserDTO;
 import web.dto.LoginRequestDTO;
 import web.dto.LoginResponseDTO;
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,9 +1,6 @@
 package web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web.dto.UserResponseDTO;
 import web.service.UserService;
 
